e4ccfb87ee60a5fd9ea5543f74e71f05fd9183fe
Mayonesa357 <djimmor2012@g.educaand.es>
2024-09-29 21:01:05 +0200

    relacion 5 terminada

--- relacion1-5/funciones.js
+++ relacion1-5/funciones.js
@@ -1,14 +1,28 @@
-function onlyUniques (...valores) {
-    let array1 = []
-    for (let i = 0; i<valores.length; i++) {
-        for (let j=0;j<array1,length;j++) {
+function onlyUniques(...valores) {
+    let array1 = [];
 
-            if (valores.at(i) == array1.at(j)) {
-                array1.push(valores.at(i))
-            } else {
+    for (let i = 0; i < valores.length; i++) {
+        let noRepetido = true;
 
+        for (let j = 0; j < array1.length; j++) {
+            if (valores[i] === array1[j]) {
+                noRepetido = false;
+                break;
             }
         }
+
+        if (noRepetido) {
+            array1.push(valores[i]);
+        }
     }
-    document.write(array1)
+
+    document.write(array1);
+}
+
+function squareAndSum (...numeros){
+    let suma = 0
+    for (let i = 0; i<numeros.length; i++) {
+        suma = suma + (numeros[i]*numeros[i])
+    }
+    document.write(suma)
 }
