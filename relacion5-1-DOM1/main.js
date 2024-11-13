6fad8b74c1cd99aba5b6b08a4ca518cc7c10d9e9
Mayonesa357 <djimmor2012@g.educaand.es>
2024-11-13 19:03:17 +0100

    Fin de la relacion 5-1-Dom1

--- relacion5-1-DOM1/main.js
+++ relacion5-1-DOM1/main.js
@@ -3,35 +3,41 @@ import { arrArticulos } from "./modulos/articulos.mjs";
 //array de arrow funtions
 
 function retornarArticulos() {
-	let tabla = document.createElement("table");
-  let contador1 = 0
+	let tabla = document.getElementById("tabla");
+
+  let cabecera = document.getElementById("cabecera")
+
+  let encabezados = tabla.createTHead()
+  encabezados.appendChild(cabecera)
+
 	arrArticulos.forEach((articulo) => {
 		let propiedad = Object.keys(articulo);
 		let contador2 = 0;
-		let row = tabla.insertRow(contador1);
-
-		propiedad.forEach((element) => {
-      let celda = row.insertCell(contador2)
-
-      if (contador2 === 5) {
-        celda.innerHTML = '<img src="' + propiedad[element] + '"/>'
-      }
-
-      if (contador2 === 6) {
-        celda.innerHTML = articulo[element].rate
-        celda = row.insertCell(7)
-        celda.innerHTML = articulo[element].count
-        
-      } else {
-        celda.innerHTML = articulo[element]
-      }
-      contador2++
-      
-		})
+		let row = tabla.insertRow(-1);
     
-    contador1++  
+		propiedad.forEach((element) => {
+      let celda = row.insertCell(contador2);
+      
+			if (contador2 === 5) {
+        celda.innerHTML = '<img  src="' + articulo[element] + '" style="width: 100px"/>';
+        celda.style.width = "30px"
+			} else if (contador2 === 6) {
+        celda.innerHTML = articulo[element].rate;
+				let celda2 = row.insertCell(contador2 + 1);
+				celda2.innerHTML = articulo[element].count;
+				contador2++;
+        celda2.style.border = "solid gray"
+        celda2.style.textAlign = "center"
+			} else {
+        celda.innerHTML = articulo[element];
+			}
+			contador2++;
+      celda.style.border = "solid gray"
+      celda.style.textAlign = "center"
+		});
 	});
-  document.body.appendChild(tabla)
+
+  tabla.style.border = "solid black"
 }
 
 retornarArticulos();
