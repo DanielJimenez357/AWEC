5f56bd0f73c9cfc5a7bccc5aa2a917a7fa06a6cb
Daniel <daniel05jimenez357@gmail.com>
2024-11-25 12:46:40 +0100

    avances en la relacion 4-1-eventos

--- relacion4-1-Eventos/funciones.js
+++ relacion4-1-Eventos/funciones.js
@@ -1,11 +1,17 @@
 const click = () => {
-    document.addEventListener("click", () => alert("Hola"))
-}
+  document.addEventListener("click", () => alert("Hola"));
+};
 
-const updatePosition  = (event ,positionY, positionX) => {
-    
-    positionY.innerHTML = event.clientY
-    positionX.innerHTML = event.clientX
-}
+const updatePosition = (event, positionY, positionX) => {
+  positionY.innerHTML = event.clientY;
+  positionX.innerHTML = event.clientX;
+};
 
-export {click, updatePosition}
+const timePassed = () => {
+  let clock = setInterval(() => {
+    time += 1
+  }, 1000);
+  onload()
+};
+
+export { click, updatePosition, timePassed };
